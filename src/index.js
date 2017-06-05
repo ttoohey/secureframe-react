19e5ad6c37975eab1775e47b7e29f8295ee11a63
Tim Toohey <tim@gency.com.au>
2017-06-05 18:32:46 +1000

    change to using preauthorisation
    these seems to be the only way to get a response that contains details
    of the card being used

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import moment from 'moment'
 import uuid from 'uuid'
 import { Base64 } from 'js-base64'
 import 'react-popupbox/dist/react-popupbox.css'
+import './secureframe-react.css'
 
 function mapHidden (inputs) {
   return Object.entries(inputs).map(([n, v]) => {
@@ -31,12 +32,13 @@ class SecureFrame extends Component {
     super(props)
     this.content = null
     this.state = {
-      txn_type: '8',
+      txn_type: '1',
       store_type: 'payor',
       payor: null,
       buffer: null,
       fingerprint: null,
       fp_timestamp: null,
+      error: null
     }
     this.receiveMessage = this.receiveMessage.bind(this)
   }
@@ -53,26 +55,45 @@ class SecureFrame extends Component {
     if (!event.data || event.data.source !== 'secureframe') {
       return
     }
+    const { store_type, fp_timestamp } = this.state
     let response = event.data.payload
     if (event.data.encoding === 'base64') {
       response = JSON.parse(Base64.decode(response))
-      if (['00', '08', '11'].includes(response.strescode)) {
-        response.subject = `payor|${response.payor}|${this.state.fp_timestamp}|${response.summarycode}`
+      if (response.summarycode === '1') {
+        if (store_type === '8') {
+          response.subject = `${store_type}|${response.payor}|${fp_timestamp}|${response.summarycode}`
+        } else {
+          response.subject = `${response.refid}|${response.amount}|${response.timestamp}|${response.summarycode}`
+        }
+        (this.props.onPaymentApproved || this.props.onPayment)(response)
+        this.closePopupbox()
+      } else if (response.summarycode === '2' || response.summarycode === '3') {
+        (this.props.onPaymentDeclined || this.props.onPayment)(response)
+        PopupboxManager.update({ content: this.renderError(response.restext) })
       } else {
-        response.subject = `${response.refid}|${response.amount}|${response.timestamp}|${response.summarycode}`
+        (this.props.onPaymentCancelled || this.props.onPayment)(response)
+        this.closePopupbox()
       }
+    } else {
+      this.props.onPayment && this.props.onPayment(response)
+      this.closePopupbox()
     }
-    this.props.onPayment(response)
-    this.closePopupbox()
   }
   
   makeFingerprint () {
     const { txn_type, store_type } = this.state
     const payor = this.props.payor || uuid.v4()
+    const primary_ref = this.props.reference || uuid.v4()
+    const amount = this.props.amount || 100
     const fp_timestamp = moment.utc().format("YYYYMMDDHHMMSS");
-    const buffer = `${txn_type}|${store_type}|${payor}`
-    return this.props.onSignPayment(`${buffer}|${fp_timestamp}`)
-      .then(fingerprint => this.setState({ buffer, payor, fingerprint, fp_timestamp }))
+    let subject
+    if (txn_type === '8') {
+      subject = `${txn_type}|${store_type}|${payor}|${fp_timestamp}`
+    } else {
+      subject = `${txn_type}|${primary_ref}|${amount}|${fp_timestamp}`
+    }
+    return this.props.onSignPayment(subject)
+      .then(fingerprint => this.setState({ primary_ref, payor, amount, fp_timestamp, fingerprint }))
   }
   
   handleClosed () {
@@ -83,7 +104,7 @@ class SecureFrame extends Component {
   openPopupbox () {
     this.makeFingerprint().then(() => {
       PopupboxManager.open({
-        content: this.content,
+        content: this.renderContent(),
         config: {
           fadeIn: true,
           fadeInSpeed: 300,
@@ -93,13 +114,22 @@ class SecureFrame extends Component {
   }
   
   closePopupbox () {
+    this.content = null
+    this.setState({ error: null, fingerprint: null })
     PopupboxManager.close()
   }
   
+  reset () {
+    this.content = null
+    this.setState({ error: null, fingerprint: null })
+    this.makeFingerprint().then(() => {
+      PopupboxManager.update({
+        content: this.renderContent()
+      })
+    })
+  }
+  
   render () {
-    if (this.state.fingerprint !== null && this.content === null) {
-      this.content = this.renderContent()
-    }
     const button = React.cloneElement(
       this.props.children || <button className={this.props.buttonClassName} style={this.props.buttonStyle}>{this.props.buttonLabel || 'Set Card'}</button>,
       {[this.props.triggerEvent || 'onClick'] : () => this.openPopupbox()}
@@ -115,31 +145,61 @@ class SecureFrame extends Component {
     
   }
   
+  renderError (error) {
+    let buttons = this.props.errorButtons
+    if (!buttons) {
+      buttons = [
+        <button key={0}>Retry</button>,
+        <button key={1}>Close</button>
+      ]
+    }
+    buttons[0] = React.cloneElement(buttons[0], {
+      [this.props.triggerEvent || 'onClick'] : () => this.reset()
+    })
+    buttons[1] = React.cloneElement(buttons[1], {
+      [this.props.triggerEvent || 'onClick'] : () => this.closePopupbox()
+    })
+    return (
+      <div className='error'>
+        <label>Transaction declined</label>
+        <div className='message'>
+          <p>
+            We could not complete the transaction due to the following:
+          </p>
+          <span>{error}</span>
+          <p>
+            Please check your card details and try again.
+          </p>
+        </div>
+        <div className='buttons'>{buttons}</div>
+      </div>
+    )
+  }
+  
   renderContent () {
-    const { txn_type, store_type, payor, fingerprint, fp_timestamp } = this.state
-    const { merchantId, title, image, referenceName, cardTypes, reference, template, returnUrl, styleUrl } = this.props
-    const amount = this.props.amount || '100'
+    const { txn_type, store_type, primary_ref, payor, amount, fingerprint, fp_timestamp } = this.state
+    const { merchantId, title, image, referenceName, cardTypes, template, returnUrl, styleUrl } = this.props
     
     const inputs = {
       bill_name: 'transact',
-      title,
-      primary_ref_name: referenceName || 'Customer',
-      primary_ref: reference,
-      page_header_image: image,
       merchant_id: merchantId,
       txn_type,
+      store_type,
+      primary_ref,
+      payor,
       amount,
+      fp_timestamp,
+      fingerprint,
       store: 'yes',
       display_receipt: 'false',
       confirmation: 'false',
-      store_type,
-      payor,
-      fp_timestamp,
-      fingerprint,
-      return_url: returnUrl || 'https://try.gency.com.au/tim/secureframe-react/',
       card_types: cardTypes ? cardTypes.join('|') : undefined,
+      title,
+      primary_ref_name: referenceName || 'Customer',
+      page_header_image: image,
       template: template || 'responsive',
       page_style_url: styleUrl || 'https://try.gency.com.au/tim/secureframe-react/style.css',
+      return_url: returnUrl || 'https://try.gency.com.au/tim/secureframe-react/',
     }
 
     const transactionUrl = this.props.transactionUrl || 
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-  entry: './index.js',
+  entry: './src/index.js',
   output: {
     filename: 'secureframe-react.js',
     path: path.resolve(__dirname, 'dist'),
